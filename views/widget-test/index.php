dcf63b1a811fb6c6eee4c30cd4b416745f4f4bf0
Nuwak <mr.igorr@gmail.com>
2016-01-05 12:59:07 +0300

    Адаптивный дизайн в Yii2. Bootstrap разметка страниц. phpNT
    https://www.youtube.com/watch?v=Z7NBVv1L9_c

--- views/widget-test/index.php
+++ views/widget-test/index.php
@@ -8,22 +8,40 @@
  */
 use yii\helpers\Html;
 use yii\helpers\Url;
-
-echo Html::a(
-    'Передать сюда id = 123',
-    Url::to(['widget-test/index','id' => '123'])
-);
-
-if(isset($_GET['id']))
-    echo "<p>$_GET[id]</p>";
-
-echo "<br/>";
-
-echo Html::a(
-    'Найти статьи за 2015 год',
-    Url::to([
-        'main/search',
-        'search' => 'статья',
-        'year' => '2015'
-    ])
-);
+?>
+<div class="row">
+    <div class="col-md-6" style="border: 1px solid #f0ad4e">
+        левая колонка
+        <div class="row">
+            <div class="col-md-4" style="border: 1px solid #f0ad4e">
+                <?php
+                echo Html::a(
+                    'Передать сюда id = 123',
+                    Url::to(['widget-test/index','id' => '123'])
+                );?>
+            </div>
+            <div class="col-md-4" style="border: 1px solid #f0ad4e">
+                2
+                <?php
+                if(isset($_GET['id']))
+                    echo "<p>$_GET[id]</p>";
+                ?>
+            </div>
+            <div class="col-md-4" style="border: 1px solid #f0ad4e">
+                <?php
+                echo Html::a(
+                    'Найти статьи за 2015 год',
+                    Url::to([
+                        'main/search',
+                        'search' => 'статья',
+                        'year' => '2015'
+                    ])
+                );
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6" style="border: 1px solid #f0ad4e">
+        правая колонка
+    </div>
+</div>
