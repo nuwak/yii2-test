8353e08107f24212ad48a48a12c3db36e6309f79
Nuwak <mr.igorr@gmail.com>
2016-01-07 13:21:03 +0300

    Модальные окна в Yii2. Modal виджет Bootstrap. phpNT

--- views/widget-test/index.php
+++ views/widget-test/index.php
@@ -9,6 +9,8 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\bootstrap\Nav;
+use yii\bootstrap\Modal;
+use yii\bootstrap\ActiveForm;
 ?>
 <div class="row">
     <div class="col-md-6" style="border: 1px solid #f0ad4e">
@@ -62,21 +64,21 @@ use yii\bootstrap\Nav;
     <div class="col-md-6" style="border: 1px solid #f0ad4e">
         Права колонка
         <div class="row">
-            <div class="col-md-4" style="border: 1px solid #f0ad4e">
+            <div class="col-md-3" style="border: 1px solid #f0ad4e">
                 <?php
                 echo Html::a(
                     'Передать сюда id = 123',
                     Url::to(['widget-test/index','id' => '123'])
                 );?>
             </div>
-            <div class="col-md-4" style="border: 1px solid #f0ad4e">
+            <div class="col-md-3" style="border: 1px solid #f0ad4e">
                 2
                 <?php
                 if(isset($_GET['id']))
                     echo "<p>$_GET[id]</p>";
                 ?>
             </div>
-            <div class="col-md-4" style="border: 1px solid #f0ad4e">
+            <div class="col-md-3" style="border: 1px solid #f0ad4e">
                 <?php
                 echo Html::a(
                     'Найти статьи за 2015 год',
@@ -86,6 +88,71 @@ use yii\bootstrap\Nav;
                         'year' => '2015'
                     ])
                 );
+                ?>
+            </div>
+            <div class="col-md-3" style="border: 1px solid #f0ad4e">
+                <?php
+                echo Html::a(
+                    'Модальное окно',
+                    ['#'],
+                    [
+                        'data-toggle' => 'modal',
+                        'data-target' => '#search',
+                        'class' => 'btn btn-warning',
+                    ]
+
+                );
+
+                    Modal::begin([
+                        'size' => 'modal-lg',
+                        'options' => [
+                            'id' => 'search'
+                        ],
+                        'header' => '<h2>Заголовок</h2>',
+//                        'toggleButton' => [
+//                            'label' => 'Окно',
+//                            'tag' => 'button',
+//                            'class' => 'btn btn-danger'
+//                        ],
+                        'footer' => 'Низ окна'
+                    ]);
+
+                ActiveForm::begin([
+                    'action' => ['/найти'],
+                    //                'method' => 'post',
+                    'method' => 'get',
+                    'options' => [
+                        'class' => ''
+                    ]
+                ]);
+                echo '<div class="input-group">';
+                echo Html::input(
+                    'type:text',
+                    'search',
+                    '',
+                    [
+                        'placeholder'=>'Найти...',
+                        'class' => 'form-control'
+                    ]
+                );
+
+                echo"</div>";
+
+                echo '<div class="input-group">';
+                echo Html::submitButton(
+                    '<span class="glyphicon glyphicon-search"></span>',
+                    [
+                        'class' => 'btn btn-success',
+                        //                        'onClick' => 'window.location.href = this.form.action + "-" + this.form.search.value.replace(/\s+/,"_") + ".html"',
+//                    'onClick' => 'window.location.href = this.form.action + "-" + this.form.search.value + ".html"',
+                        'onClick' => 'window.location.href = this.form.action + "-" + this.form.search.value.replace(/[^\w\а-яё\А-ЯЁ]+/g, "_") + ".html";'
+                    ]
+                );
+                echo"</div>";
+                ActiveForm::end();
+
+
+                Modal::end();
                 ?>
             </div>
         </div>
